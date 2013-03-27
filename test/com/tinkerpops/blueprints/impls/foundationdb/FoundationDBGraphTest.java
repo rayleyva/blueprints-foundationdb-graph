933dc353fbcd85c22da2a930ae40b83ce4de9114
Will Wilson <wilson.william@gmail.com>
2013-03-27 15:44:48 -0400

    add test suite

---
+++ test/com/tinkerpops/blueprints/impls/foundationdb/FoundationDBGraphTest.java
@@ -0,0 +1,93 @@
+package com.tinkerpops.blueprints.impls.foundationdb;
+
+import static org.junit.Assert.*;
+
+import java.lang.reflect.Method;
+
+import com.tinkerpop.blueprints.*;
+import com.tinkerpop.blueprints.impls.GraphTest;
+import com.tinkerpop.blueprints.impls.foundationdb.FoundationDBGraph;
+import com.tinkerpop.blueprints.util.io.gml.GMLReaderTestSuite;
+import com.tinkerpop.blueprints.util.io.graphml.GraphMLReaderTestSuite;
+import com.tinkerpop.blueprints.util.io.graphson.GraphSONReaderTestSuite;
+
+import org.junit.Test;
+
+
+public class FoundationDBGraphTest extends GraphTest {
+
+    public void testVertexTestSuite() throws Exception {
+        this.stopWatch();
+        doTestSuite(new VertexTestSuite(this));
+        printTestPerformance("VertexTestSuite", this.stopWatch());
+    }
+
+    public void testEdgeTestSuite() throws Exception {
+        this.stopWatch();
+        doTestSuite(new EdgeTestSuite(this));
+        printTestPerformance("EdgeTestSuite", this.stopWatch());
+    }
+
+    public void testGraphTestSuite() throws Exception {
+        this.stopWatch();
+        doTestSuite(new GraphTestSuite(this));
+        printTestPerformance("GraphTestSuite", this.stopWatch());
+    }
+
+    public void testKeyIndexableGraphTestSuite() throws Exception {
+        this.stopWatch();
+        doTestSuite(new KeyIndexableGraphTestSuite(this));
+        printTestPerformance("KeyIndexableGraphTestSuite", this.stopWatch());
+    }
+
+    public void testIndexableGraphTestSuite() throws Exception {
+        this.stopWatch();
+        doTestSuite(new IndexableGraphTestSuite(this));
+        printTestPerformance("IndexableGraphTestSuite", this.stopWatch());
+    }
+
+    public void testIndexTestSuite() throws Exception {
+        this.stopWatch();
+        doTestSuite(new IndexTestSuite(this));
+        printTestPerformance("IndexTestSuite", this.stopWatch());
+    }
+
+    public void testGraphMLReaderTestSuite() throws Exception {
+        this.stopWatch();
+        doTestSuite(new GraphMLReaderTestSuite(this));
+        printTestPerformance("GraphMLReaderTestSuite", this.stopWatch());
+    }
+
+    public void testGMLReaderTestSuite() throws Exception {
+        this.stopWatch();
+        doTestSuite(new GMLReaderTestSuite(this));
+        printTestPerformance("GMLReaderTestSuite", this.stopWatch());
+    }
+
+    public void testGraphSONReaderTestSuite() throws Exception {
+        this.stopWatch();
+        doTestSuite(new GraphSONReaderTestSuite(this));
+        printTestPerformance("GraphSONReaderTestSuite", this.stopWatch());
+    }
+
+    public Graph generateGraph() {
+        return new FoundationDBGraph();
+    }
+
+    public void doTestSuite(final TestSuite testSuite) throws Exception {
+        String doTest = System.getProperty("testTinkerGraph");
+        if (doTest == null || doTest.equals("true")) {
+            for (Method method : testSuite.getClass().getDeclaredMethods()) {
+                if (method.getName().startsWith("test")) {
+                    System.out.println("Testing " + method.getName() + "...");
+                    method.invoke(testSuite);
+                }
+            }
+        }
+    }
+
+	@Override
+	public Graph generateGraph(String arg0) {
+		return new FoundationDBGraph();
+	}
+}
